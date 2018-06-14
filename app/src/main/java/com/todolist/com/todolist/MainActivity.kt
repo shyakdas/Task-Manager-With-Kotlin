4ef59f589d6a740c168ddfe9252c4ab34232060b
Shyak Das <shayak@adview.com>
2018-06-14 18:00:15 +0530

    [FIX] Toolbar fix

--- app/src/main/java/com/todolist/com/todolist/MainActivity.kt
+++ app/src/main/java/com/todolist/com/todolist/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.todolist.com.todolist
 
 import android.os.Bundle
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
+import com.todolist.com.todolist.search.SearchFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        search.setOnClickListener(this)
+        sort.setOnClickListener(this)
+        new_note.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.search -> {
+                var searchFragment = SearchFragment()
+                supportFragmentManager.inTransaction { add(R.id.top, searchFragment) }
+            }
+            R.id.sort -> Toast.makeText(this, "Sort", Toast.LENGTH_LONG).show()
+            R.id.new_note -> Toast.makeText(this, "New Note", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> Unit) {
+        val fragmentTransaction = beginTransaction()
+        fragmentTransaction.func()
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
---
+++ app/src/main/java/com/todolist/com/todolist/search/SearchFragment.kt
@@ -0,0 +1,16 @@
+package com.todolist.com.todolist.search
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.todolist.com.todolist.R
+
+class SearchFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view: View = inflater!!.inflate(R.layout.search, container, false)
+        return view
+    }
+}
