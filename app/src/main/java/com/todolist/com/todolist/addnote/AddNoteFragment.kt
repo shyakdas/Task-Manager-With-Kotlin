8761f81e60b5ba5fa957a60e7e2a7e4ebe23c684
Shyak Das <shayak@adview.com>
2018-06-15 21:53:08 +0530

    [Desgin]Add Note Desgin

--- app/src/main/java/com/todolist/com/todolist/addnote/AddNoteFragment.kt
+++ app/src/main/java/com/todolist/com/todolist/addnote/AddNoteFragment.kt
@@ -1,16 +1,15 @@
 package com.todolist.com.todolist.addnote
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
+import android.support.design.widget.BottomSheetDialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.todolist.com.todolist.R
 
-class AddNoteFragment : Fragment() {
+class AddNoteFragment : BottomSheetDialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view: View = inflater!!.inflate(R.layout.sort, container, false)
-        return view
+        return inflater!!.inflate(R.layout.addnote, container, false)
     }
 }
