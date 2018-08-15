4af4e124ff032c4ca43ea3be0c6b86bc771c9e72
Shyak Das <shayak@adview.com>
2018-08-15 20:24:44 +0530

    [Implementation]OnBackpressed handled

--- app/src/main/java/com/todolist/com/todolist/search/SearchFragment.kt
+++ app/src/main/java/com/todolist/com/todolist/search/SearchFragment.kt
@@ -2,19 +2,51 @@ package com.todolist.com.todolist.search
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.todolist.com.todolist.R
+import kotlinx.android.synthetic.main.search.*
 
-class SearchFragment : Fragment() {
+class SearchFragment : Fragment(), View.OnClickListener {
+
+    companion object {
+        val TAG: String = SearchFragment.javaClass.name
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view: View = inflater!!.inflate(R.layout.search, container, false)
+        val mBackPressed: ImageView = view.findViewById(R.id.search_main)
+        mBackPressed.setOnClickListener(this)
         loadData()
         return view
     }
 
     private fun loadData() {
+        search_note?.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.e(TAG, "onTextChanged==" + s.toString())
+            }
+
+        })
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.search_main -> {
+                activity?.onBackPressed()
+            }
+        }
     }
 }
